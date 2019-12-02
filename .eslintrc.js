a8108db3b8de349fcffbdf7e6f37c4abc3616165
zatsepa <artem.zatsepa@nixsolutions.com>
2019-12-02 17:14:21 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'babel',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/jsx-one-expression-per-line": [0],
+    "react/state-in-constructor": [0],
+    "jsx-a11y/click-events-have-key-events": [0],
+    "jsx-a11y/no-noninteractive-element-interactions": [0],
+    "react/static-property-placement": ["warn", "static public field"],
+    "no-console": [0],
+    "operator-linebreak": [0],
+  },
+};
